251e88676336a6d53d912a0eb281fdb2c65e1d0e
Oluwaseun Jayeoba <seunjay92@gmail.com>
2021-08-11 19:59:03 +0100

    custom text component

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import AppText from './app/components/AppText';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <>
-      {/* <WelcomeScreen /> */}
-      <ViewImageScreen />
-    </>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <AppText>I love React Native!</AppText>
+    </View>
   );
 }
 
---
+++ app/components/AppText.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+
+const AppText = ({ children }) => {
+  return (
+    <View>
+      <Text style={styles.text}>{children}</Text>
+    </View>
+  );
+};
+
+export default AppText;
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
+  },
+});
