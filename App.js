35479b626d583147ba68b888c750f4e83898f311
Oluwaseun Jayeoba <seunjay92@gmail.com>
2021-08-12 19:53:28 +0100

    create reusable icon component

--- App.js
+++ App.js
@@ -3,13 +3,19 @@ import { StyleSheet, View } from 'react-native';
 import AppButton from './app/components/AppButton';
 import AppText from './app/components/AppText';
 import Card from './app/components/Card';
+import Icon from './app/components/Icon';
+import Screen from './app/components/Screen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name='email'  />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
---
+++ app/components/Icon.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const Icon = ({
+  name,
+  size = 40,
+  backgroundColor = '#000',
+  iconColor = '#fff',
+}) => {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        backgroundColor,
+        borderRadius: size / 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+      }}
+    >
+      <MaterialCommunityIcons name={name} size={size * 0.5} color={iconColor} />
+    </View>
+  );
+};
+
+export default Icon;
+
+const styles = StyleSheet.create({});
