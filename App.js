820a8f5dc2629fc0c36b455c1db060924b28452d
Oluwaseun Jayeoba <seunjay92@gmail.com>
2021-08-12 14:01:42 +0100

    use close and delete icons on view image screen

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
